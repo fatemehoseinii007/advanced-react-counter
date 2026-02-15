cc8c43b134dac594430b1bf560701a76b8f68332
Fatemeh Sadat Hoseini <fatemehoseinii007@gmail.com>
2026-02-15 19:26:28 +0330

    create counter UI with custom colors and font

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const App:React.FC = () => {
+return(
+  <>
+  <div className="min-h-screen flex justify-center items-center bg-background">
+    <div className="h-auto p-5 rounded-sm bg-card">
+      <div>
+      <p className="text-4xl text-center vazirmatn">کانتر من</p>
+      </div>
+    </div>
+  </div>
+  </>
+) 
+
+}
+export default App;
