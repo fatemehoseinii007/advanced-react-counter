1dbaaa109823350fd9b9eda36e136c8d5616d832
Fatemeh Sadat Hoseini <fatemehoseinii007@gmail.com>
2026-02-18 23:34:21 +0330

    add counter functionality with useState and min/max validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,38 @@ import { useState } from "react";
 import { PlusIcon, MinusIcon, ArrowPathIcon ,PlayIcon ,CursorArrowRippleIcon} from '@heroicons/react/24/outline';
 
 const App:React.FC = () => {
+
+const [counter, setcounter] = useState<number>(5);
+
+function increment (){
+  setcounter(prev => Math.min(20, prev + 1));
+}
+
+function decrement(){
+  setcounter(prev => Math.max(0, prev - 1));
+}
+
+function reset(){
+  setcounter(5);
+}
+
 return(
   <>
   <div className="min-h-screen flex justify-center items-center bg-background">
     <div className="h-auto p-9 rounded-sm bg-card shadow-shadow-[0_20px_25px_-5px_rgba(0,0,0,0.1)]">
       <div>
       <p className="text-3xl text-center text-header-text vazirmatn mb-7">کانتر من</p>
-      <p className="font-extrabold text-8xl text-center text-head-number">4</p>
+      <p className="font-extrabold text-8xl text-center text-head-number">{counter}</p>
       <div className="items-center justify-center flex gap-10 mt-10">
-        <button className="rounded-full p-5 bg-first-button">
+        <button onClick={addMinusToCounter} className="rounded-full p-5 bg-first-button">
           <MinusIcon className="w-10 h-10"></MinusIcon>
         </button>
-        <button className="bg-second-button rounded-full py-6 px-8">
+        <button onClick={addResetButton} className="bg-second-button rounded-full py-6 px-8">
           <ArrowPathIcon className="inline w-7 h-7"></ArrowPathIcon>
           <span className="vazirmatn text-xl ml-1.5">ریست</span>
         </button>
-        <button className="bg-third-button rounded-full p-5 ">
-          <PlusIcon className=" w-10 h-10 "></PlusIcon>
+        <button onClick={addPlusToCounter} className="bg-third-button rounded-full p-5 ">
+        <PlusIcon className=" w-10 h-10 "></PlusIcon>
         </button>
       </div>
 
@@ -43,8 +58,7 @@ return(
       <div className="text-center mt-8">
         <small className="vazirmatn text-small-tag">محدوده: 0 تا 20 | گام: 2</small>
       </div>
-ّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّ
-      </div>
+  </div>
     </div>
   </div>
   </>
